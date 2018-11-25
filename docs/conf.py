ba191de25551e458e0ebaff565ce9ab496d49a27
Antonizoon <antonizoon@bibanon.org>
2018-11-25 17:44:26 -0500

    rename from py8chan

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# py8chan documentation build configuration file, created by
+# pyvichan documentation build configuration file, created by
 # sphinx-quickstart on Fri Dec  5 08:19:29 2014.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -41,8 +41,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'py8chan'
-copyright = u'2014, Antonizoon (Antonizoon Overtwater)'
+project = u'pyvichan'
+copyright = u'2014, Antonizoon Overtwater'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -174,7 +174,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'py8chandoc'
+htmlhelp_basename = 'pyvichandoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -193,8 +193,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'py8chan.tex', u'py8chan Documentation',
-   u'Antonizoon (Antonizoon Overtwater)', 'manual'),
+  ('index', 'pyvichan.tex', u'pyvichan Documentation',
+   u'Antonizoon Overtwater', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -223,8 +223,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'py8chan', u'py8chan Documentation',
-     [u'Antonizoon (Antonizoon Overtwater)'], 1)
+    ('index', 'pyvichan', u'pyvichan Documentation',
+     [u'Antonizoon Overtwater'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -237,8 +237,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'py8chan', u'py8chan Documentation',
-   u'Antonizoon (Antonizoon Overtwater)', 'py8chan', 'One line description of project.',
+  ('index', 'pyvichan', u'pyvichan Documentation',
+   u'Antonizoon Overtwater', 'pyvichan', 'One line description of project.',
    'Miscellaneous'),
 ]
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """8chan/vichan Python Library.
 
-py8chan is a Python library that gives access to the 8chan/vichan API
+pyvichan is a Python library that gives access to the 8chan/vichan API
 and an object-oriented way to browse and get board and thread
 information quickly and easily.
 
@@ -16,17 +16,17 @@ the LICENSE file for more details.
 from setuptools import setup
 
 setup(
-    name='py8chan',
+    name='vichan',
     version='0.0.1',
-    description=("Python 8chan/vichan API Wrapper. Based on BASC-py4chan by the Bbiliotheca Anonoma"),
+    description=("Python vichan API Wrapper. Based on BASC-py4chan by the Bbiliotheca Anonoma"),
     long_description=open('README.rst').read(),
     license=open('LICENSE').read(),
-    author='Antonizoon (Antonizoon Overtwater)',
+    author='Antonizoon Overtwater',
     author_email='antonizoon@bibanon.org',
     url='http://github.com/bibanon/py8chan',
-    packages=['py8chan'],
+    packages=['pyvichan'],
     package_dir={
-        'py8chan': 'py8chan',
+        'pyvichan': 'pyvichan',
     },
     package_data={'': ['README.rst', 'LICENSE']},
     install_requires=['requests >= 1.0.0'],
